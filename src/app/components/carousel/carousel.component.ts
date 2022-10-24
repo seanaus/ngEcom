c920b8f6047708ace3c66b47857ace665cb723ac
seanaus <94388567+seanaus@users.noreply.github.com>
2022-10-24 16:44:35 +0100

    finnish off carousel with animations

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CarouselService } from 'src/services/carousel.service';
-
-
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
-  // items=[];
+ 
   carousel: any;
   slide: number = 0;
   slides: number = 0;
@@ -26,17 +24,13 @@ export class CarouselComponent implements OnInit {
         this.carousel = carousel;
         this.slides = (this.carousel.items.length > 0) ? this.carousel.items.length -1  : 0
     })
-    console.log(`Current Slide is ${this.slide}`)
-
   }
 
   prev() {
     this.slide= this.slide > 0 ?  this.slide -1 : this.slides;
-    // console.log(`Current Slide is ${this.slide}`)
   }
   next() {
     this.slide= this.slide + 1 <= this.slides ? this.slide+1 : 0;
-    // console.log(`Current Slide is ${this.slide}`)
   }
   autoSlide() {
     this.slide= this.slide + 1 <= this.slides ? this.slide + 1 : 0;
@@ -45,5 +39,7 @@ export class CarouselComponent implements OnInit {
     const target = event.target as HTMLImageElement;
     this.slide = +target.id;
   }
-
+  slideSelected(idx: number) {
+    return (idx === this.slide) ? true : false;
+  }
 }
